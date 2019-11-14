93bac8009d5ee376ff90639cb7802bb0c9923c08
Gustaf Colliander <gusco933@student.liu.se>
2019-11-14 18:22:12 +0100

    Ändrade klasstrukturen. Node ska tydligen vara en inre klass till Sorted_List. ListHandler verkade onödig så tog bort den.

--- Sorted_List/Sorted_List.h
+++ Sorted_List/Sorted_List.h
@@ -10,13 +10,17 @@ class Node;
 //=================================
 // included dependencies
 #include "ListHandler.h"
+#include <string>
 
 //=================================
 
 class Sorted_List : public ListHandler  {
 public:
   Sorted_List() = default;
-  //Att göra: lägg till konstruktor som tar ett godtyckligt antal parametrar
+  //Att göra: lägg till konstruktor som tar ett godtyckligt antal parametrar och gör om till sträng och skickar till ListHandler konstruktor
+  // tips: använd initializer_list som konstruktor-argument
+
+  //tips: Ha Node som inre klass under private
   Sorted_List(Sorted_List const & old_list);
   Sorted_List& operator=(Sorted_List const&);
   virtual ~Sorted_List() {}
@@ -27,8 +31,25 @@ public:
   virtual int get_element(int) const;
   virtual int get_length() const;
   virtual bool is_empty() const;
-
+  std::string input{"1 7 3 2 5"};
   Node* first_node{};
+
+private:
+  class Node : public Sorted_List  {
+  public:
+    Node() = default;
+    Node(double value_, Node* next_node_)
+    : value{value_}, next_node{next_node_}  {}
+    Node(Node const & old_node);
+    Node& operator=(Node const&);
+    virtual ~Node() {}
+
+    virtual bool is_empty() const;
+
+    double value{};
+    Node* next_node{};
+
+  };
 
 };
 #endif
